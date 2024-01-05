bed1bbf6e35426ea9892f4419c343df982320f93
imran hassan <hassanimran114@gmail.com>
2024-01-05 23:54:46 +0530

    added unique constraint and started cityrepo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express=require('express');
+
 const {PORT}=require('./config/ServerConfig')
+const cityrepository=require('./repository/city-repository');
 const app=express();
 
 app.listen(PORT, ()=>{
     console.log(`server is connect ${PORT}`)
-   //console.log(process.env)
+   
 })
