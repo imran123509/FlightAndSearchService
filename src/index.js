784c3a8bcf3d4cc2e3e04524f0d90a28946b2d7c
imran hassan <hassanimran114@gmail.com>
2024-01-07 15:16:18 +0530

    added all about api

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const app=express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/api', ApiRoutes);
+app.use('/api', ApiRoutes);           
 
 app.listen(PORT, ()=>{
     console.log(`server is connect ${PORT}`)
